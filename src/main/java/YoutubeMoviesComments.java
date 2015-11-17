9c0bff73a77c55a206dcb62e1e544080f68d78cb
gmiejski <grz.miejski@gmail.com>
2015-11-17 19:33:32 +0100

    Receiving commentThreads for single movie.

--- src/main/java/YoutubeMoviesComments.java
+++ src/main/java/YoutubeMoviesComments.java
@@ -1,10 +1,11 @@
 import agh.edu.pl.youtube.YoutubeClient;
 
-import com.google.api.services.youtube.model.Comment;
+import com.google.api.services.youtube.model.CommentListResponse;
 import com.google.api.services.youtube.model.CommentThread;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by grzegorz.miejski on 13/11/15.
@@ -15,16 +16,44 @@ public class YoutubeMoviesComments {
 
 
         YoutubeClient youtubeClient = new YoutubeClient();
+        List<CommentThread> commentThreads = getCommentsThreads(movieId, youtubeClient);
 
-        List<CommentThread> commentThreads = youtubeClient.get().commentThreads().list("snippet")
-                .setVideoId(movieId)
-                .setKey(youtubeClient.apiKey())
-                .setMaxResults(10L)
-                .execute().getItems();
+        getRepliesCOunt(youtubeClient, commentThreads.get(0).getId());
 
-        commentThreads.stream().forEach(System.out::println);
+//        commentThreads.stream().map(x -> x.getReplies().getgetSnippet());
+//        getRepliesCOunt(youtubeClient, commentThreads.get(0).getSnippet().getTopLevelComment().getId());
+        List<Integer> collect = commentThreads.stream().map(x -> {
+            try {
+                return getRepliesCOunt(youtubeClient, x.getSnippet().getTopLevelComment().getId());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }).collect(Collectors.toList());
+
+//        commentThreads.get(0).getReplies().getComments();
 
         return null;
+    }
+
+    private List<CommentThread> getCommentsThreads(String movieId, YoutubeClient youtubeClient) throws IOException {
+        return youtubeClient.get().commentThreads().list("snippet")
+                .setVideoId(movieId)
+                .setKey(youtubeClient.apiKey())
+                .setMaxResults(100L)
+                .execute().getItems();
+    }
+
+    private int getRepliesCOunt(YoutubeClient youtubeClient, String parentCommentId) throws IOException {
+        CommentListResponse response = youtubeClient.get().comments().list("snippet")
+                .setKey(youtubeClient.apiKey())
+                .setParentId(parentCommentId)
+                .setTextFormat("plainText")
+                .setMaxResults(100L)
+                .execute();
+
+
+        return response.getItems().size();
     }
 
 
@@ -32,8 +61,8 @@ public class YoutubeMoviesComments {
 
 
         YoutubeMoviesComments youtubeMoviesComments = new YoutubeMoviesComments();
-        youtubeMoviesComments.getCommentsForMovie("KlujizeNNQM");
+        String movieId = "KlujizeNNQM";
+        youtubeMoviesComments.getCommentsForMovie(movieId);
 
     }
-
 }
---
+++ src/main/java/agh/edu/pl/youtube/CommentsFromThread.java
@@ -0,0 +1,11 @@
+package agh.edu.pl.youtube;
+
+/**
+ * Created by grzegorz.miejski on 14/11/15.
+ */
+public class CommentsFromThread {
+
+
+
+
+}
